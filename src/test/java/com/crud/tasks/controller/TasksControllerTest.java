2dbe3f869b120c2312fa3f93bb75576bf00a3017
KorneliaAdamczyk <adamczykkornelia1@gmail.com>
2018-06-29 14:24:29 +0200

    21.3 Tests of TaskController

--- src/test/java/com/crud/tasks/controller/TasksControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TasksControllerTest.java
@@ -11,7 +11,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
@@ -35,9 +35,6 @@ public class TasksControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    TasksController tasksController;
-
-    @MockBean
     TaskMapper taskMapper;
 
     @MockBean
@@ -47,15 +44,24 @@ public class TasksControllerTest {
     public void shouldGetTasks()throws Exception{
 
         //Given
-        List<TaskDto> list = new ArrayList<>();
-        TaskDto task1 = new TaskDto(1L, "Shopping", "Buy new shoes");
-        TaskDto task2 = new TaskDto(2L, "Learning", "Read about Spring");
-        TaskDto task3 = new TaskDto(3L, "Cooking", "Cook dinner");
+        List<TaskDto> listDto = new ArrayList<>();
+        TaskDto taskDto1 = new TaskDto(1L, "Shopping", "Buy new shoes");
+        TaskDto taskDto2 = new TaskDto(2L, "Learning", "Read about Spring");
+        TaskDto taskDto3 = new TaskDto(3L, "Cooking", "Cook dinner");
+        listDto.add(taskDto1);
+        listDto.add(taskDto2);
+        listDto.add(taskDto3);
+
+        List<Task> list = new ArrayList<>();
+        Task task1 = new Task(1L, "Shopping", "Buy new shoes");
+        Task task2 = new Task(2L, "Learning", "Read about Spring");
+        Task task3 = new Task(3L, "Cooking", "Cook dinner");
         list.add(task1);
         list.add(task2);
         list.add(task3);
 
-        when(tasksController.getTasks()).thenReturn(list);
+        when(service.getAllTasks()).thenReturn(list);
+        when(taskMapper.mapToTaskDtoList(list)).thenReturn(listDto);
 
         //When & Then
         mockMvc.perform(get("/v1/task/getTasks").contentType(MediaType.APPLICATION_JSON))
@@ -71,9 +77,11 @@ public class TasksControllerTest {
     public void shouldGetTask()throws Exception{
 
         //Given
-        TaskDto task2 = new TaskDto(2L, "Learning", "Read about Spring");
+        Task task2 = new Task(2L, "Learning", "Read about Spring");
+        TaskDto taskDto2 = new TaskDto(2L, "Learning", "Read about Spring");
 
-        when(tasksController.getTask(2L)).thenReturn(task2);
+        when(service.getTask(2L)).thenReturn(java.util.Optional.ofNullable(task2));
+        when(taskMapper.mapToTaskDto(task2)).thenReturn(taskDto2);
 
         //When & Then
         mockMvc.perform(get("/v1/task/getTask").param("taskId","2").contentType(MediaType.APPLICATION_JSON))
@@ -91,7 +99,7 @@ public class TasksControllerTest {
         mockMvc.perform(delete("/v1/task/deleteTask").param("taskId","2").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
-        Mockito.verify(tasksController,Mockito.times(1)).deleteTask(2L);
+        Mockito.verify(service,Mockito.times(1)).deleteTask(2L);
     }
 
     @Test
@@ -109,7 +117,7 @@ public class TasksControllerTest {
                 .content(jsonContent))
                 .andExpect(status().isOk());
 
-     //  Mockito.verify(tasksController,Mockito.times(1)).createTask(createdTask);
+       Mockito.verify(service,Mockito.times(1)).saveTask(any(Task.class));
 
     }
 
@@ -121,10 +129,9 @@ public class TasksControllerTest {
         TaskDto updatedTaskDto = new TaskDto(2L,"Learning","Read about tests");
         Task updateTask = new Task(2L,"Learning","Read about tests");
 
-        when(tasksController.updateTask(taskDto2)).thenReturn(updatedTaskDto);
-      //  when(taskMapper.mapToTask(updatedTaskDto)).thenReturn(updateTask);
-      //  when(service.saveTask(updateTask)).thenReturn(updateTask);
-      //  when(taskMapper.mapToTaskDto(updateTask)).thenReturn(updatedTaskDto);
+        when(taskMapper.mapToTask(any(TaskDto.class))).thenReturn(updateTask);
+        when(service.saveTask(updateTask)).thenReturn(updateTask);
+        when(taskMapper.mapToTaskDto(updateTask)).thenReturn(updatedTaskDto);
 
         Gson gson = new Gson();
         String jsonContent = gson.toJson(updatedTaskDto);
@@ -134,10 +141,10 @@ public class TasksControllerTest {
                .contentType(MediaType.APPLICATION_JSON)
                .characterEncoding("UTF-8")
                .content(jsonContent))
-               .andExpect(status().isOk());
-            //   .andExpect(jsonPath("$.id",is(2)))
-             //  .andExpect(jsonPath("$.title",is("Learning")))
-            //    .andExpect(jsonPath("$.content", is("Read about tests")));
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.id",is(2)))
+               .andExpect(jsonPath("$.title",is("Learning")))
+                .andExpect(jsonPath("$.content", is("Read about tests")));
     }
 
 
